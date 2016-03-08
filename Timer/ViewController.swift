7f418435701c8b10582c5f4e45fd03badf89657c
Anson Ciurcovich <aciurc@hotmail.com>
2016-03-08 09:20:12 -0800

    Fixed the pause button

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TimerViewController.swift
 //  Timer
 //
 //  Created by Anson Ciurcovich on 2016-01-29.
@@ -13,13 +13,16 @@ class ViewController: UIViewController {
     
     var timer = NSTimer()
     var counter = 0
+    var isPaused = false
+    var isRunning = false
     @IBOutlet var voiceEnableLabel: UILabel!
     @IBOutlet var countingLabel: UILabel!
     @IBOutlet var timeInputField: UITextField!
     @IBOutlet var voiceSwitch: UISwitch!
     
     override func viewDidLoad() {
-        super.viewDidLoad()        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,26 +31,46 @@ class ViewController: UIViewController {
     }
     
     @IBAction func startButton(sender: AnyObject) {
-        let num = Int(timeInputField.text!)
-        if num != nil && num > 0 && num < Int(INT_MAX) {
-            counter = num!
-            countingLabel.text = formatTime(counter) as String
-            timer.invalidate()
-            timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: Selector("updateCounter"), userInfo: nil, repeats: true)
-        } else {
-            countingLabel.text = "Invalid Number"
+        if isRunning == false {
+            if isPaused {
+                timer.invalidate()
+                timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: Selector("updateCounter"), userInfo: nil, repeats: true)
+                isRunning = true
+            } else {
+                let num = Int(timeInputField.text!)
+                if num != nil && num > 0 && num < Int(INT_MAX) {
+                    counter = num!
+                    countingLabel.text = formatTime(counter) as String
+                    timer.invalidate()
+                    timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: Selector("updateCounter"), userInfo: nil, repeats: true)
+                    isRunning = true
+                } else {
+                    countingLabel.text = "Invalid Number"
+                }
+            }
         }
     }
     
+    @IBAction func pauseButton(sender: AnyObject) {
+        if isRunning {
+            timer.invalidate()
+            isPaused = true
+            isRunning = false
+        }
+    }
     @IBAction func clearButton(sender: AnyObject) {
         timer.invalidate()
+        isPaused = false
+        isRunning = false
         countingLabel.text = "00:00:00"
     }
     
     func updateCounter() {
-        countingLabel.text = formatTime(--counter) as String
         if counter <= 0 {
             timer.invalidate()
+        } else {
+            countingLabel.text = formatTime(--counter) as String
+            isRunning = true
         }
         if voiceSwitch.on {
             var utterance: AVSpeechUtterance
@@ -69,9 +92,9 @@ class ViewController: UIViewController {
     }
     @IBAction func voiceEnabled() {
         if voiceSwitch.on {
-            voiceEnableLabel.text = "Voice Enabled"
+            voiceEnableLabel.text = "Countdown Enabled"
         } else {
-            voiceEnableLabel.text = "Voice Disabled"
+            voiceEnableLabel.text = "Countdown Disabled"
         }
     }
     func formatTime(interval:Int) -> NSString {
